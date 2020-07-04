7d567671826bd53f24523491ed51ff1c8a920ac1
victor-osf <victor.rocha@ifactory.com.br>
2020-07-04 17:31:00 -0300

    adding loading text to ajax call

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 (function(){
-  $('#submit-email').click(() => {
+  $('#submit-form').click((e) => {
     const email = $('#input-email').val();
     const name = $('#input-name').val();
     let regex = new RegExp(/^[a-z0-9\.\-\_]+@[a-z0-9]+[-]*[a-z0-9]*\.[a-z]+(\.[a-z]+)?$/gm);
 
     if (regex.test(email)) {
-      $('#modalEbook').modal('hide');
-
-      const data = { name, email }
-
+      $(e.target).text("Carregando...").attr("disabled", true)
       $.ajax({
         url: 'https://us-central1-links-1c9cc.cloudfunctions.net/addLead',
         dataType: 'json',
         contentType: "application/json",
         type: 'POST',
-        data: JSON.stringify(data),
+        data: JSON.stringify({ name, email }),
         success: downloadFile,
       });
     } else {  
@@ -24,6 +21,7 @@
   })
 
   const downloadFile = () => {
+    $('#modalEbook').modal('hide');
     var file_path = 'images/ebook-2.png';
     var a = document.createElement('a');
     a.href = file_path;
@@ -35,6 +33,7 @@
 
   $('#modalEbook').on('hide.bs.modal', () => {
     $('#alert-validation').fadeOut()
+    $('#submit-form').text("Download").attr("disabled", false)
     $('#input-email').val('')
     $('#input-name').val('')
   })
